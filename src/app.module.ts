b6a6587e2bc4ae236d1c38e870ef68b2aeaf032e
MolinaRomain <56554300+MolinaRomain@users.noreply.github.com>
2024-04-23 16:01:15 +0200

    DatabaseInit (#2)
    Co-authored-by: Romain Molina <romain.molina@getyooz.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigurationModule } from './configuration/configuration.module';
 import { ComponentModule } from './component/component.module';
+import { dataSourceOptions } from "../database/database.providers";
 
 @Module({
-  imports: [ConfigurationModule,  ComponentModule],
+  imports: [ConfigurationModule,  ComponentModule, TypeOrmModule.forRoot(dataSourceOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
