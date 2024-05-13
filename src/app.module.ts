9ef33f535b7f944c775b2a4a557aa5324281aae0
dakeinu <remi.falcati@ynov.com>
2024-05-13 09:48:27 +0200

    Fix Users & Cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { ConfigurationModule } from './configuration/configuration.module';
 import { ComponentModule } from './component/component.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { CacheModule, CacheInterceptor } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigurationModule,  ComponentModule, UserModule, AuthModule],
+  imports: [ConfigurationModule,  ComponentModule, UserModule, AuthModule, CacheModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
