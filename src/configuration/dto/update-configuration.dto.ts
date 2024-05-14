f96470937a5e28df35d99398a2764c8a7081f30e
fabien382 <fabien.nvn@gmail.com>
2024-05-14 15:51:14 +0200

    Refactor configuration DTOs to use arrays of components instead of individual component IDs

--- src/configuration/dto/update-configuration.dto.ts
+++ src/configuration/dto/update-configuration.dto.ts
@@ -1,18 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateConfigurationDto } from './create-configuration.dto';
+import { Component } from 'src/component/entities/component.entity';
 
 export class UpdateConfigurationDto extends PartialType(CreateConfigurationDto) {
     id: number;
     name: string;
     price: number | null;
     user_id: number | null;
-    processor_id: number | null;
-    motherboard_id: number | null;
-    gpu_id: number | null;
-    ventirad_id: number | null;
-    memory_id: number | null;
-    storage_id: number | null;
-    externaldevice_id: number | null;
-    case_id: number | null;
-    powersupply_id: number | null;
+    processors: Component[];
+    motherboards: Component[];
+    gpus: Component[];
+    ventirads: Component[];
+    memorys: Component[];
+    storages: Component[];
+    externaldevices: Component[];
+    cases: Component[];
+    powersupplys: Component[];
 }
